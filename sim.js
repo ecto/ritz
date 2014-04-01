e06ffd9655650c3c88b178ed001029c1d06d151b
Cam Pedersen <diffference@gmail.com>
2014-04-01 06:54:47 -0500

    rearrange dir structure and working simulation

--- sim.js
+++ sim.js
@@ -1,17 +1,38 @@
-var Deck = require('./deck');
-var Shoe = require('./shoe');
-var Game = require('./game');
-var Player = require('./player');
+var ritz = require('./lib')();
+var Game = ritz.Game;
+var Player = ritz.players.Base;
 
 /*
-var shoe = new Shoe();
+// Create a deck
+var deck = new ritz.Deck();
+deck.shuffle();
+console.log(deck.toString());
+console.log(deck.cards.length, 'cards');
+
+// Create a shoe
+var shoe = new ritz.Shoe();
 shoe.shuffle();
 console.log(shoe.toString());
-console.log(shoe.cards.length);
+console.log(shoe.cards.length, 'cards');
 */
 
-var game = new Game();
 var player = new Player();
 player.name = 'Alice';
-player.join(game);
-game.play();
+player.balance = 10000;
+
+var player2 = new Player();
+player2.name = 'Bob';
+player2.balance = 10000;
+
+var game;
+function newGame () {
+  game = new Game();
+  player.join(game);
+  player2.join(game);
+  game.on('over', function () {
+    newGame();
+  });
+  game.play();
+}
+
+newGame();
