9c29103d93d19518ae7156daf9a8e3c7bed27c49
Cam Pedersen <diffference@gmail.com>
2014-04-01 14:19:26 -0500

    working table and smart player

--- sim.js
+++ sim.js
@@ -1,38 +1,86 @@
 var ritz = require('./lib')();
-var Game = ritz.Game;
+var Table = ritz.Table;
 var Player = ritz.players.Base;
+var SmartPlayer = ritz.players.SmartPlayer;
 
-/*
-// Create a deck
-var deck = new ritz.Deck();
-deck.shuffle();
-console.log(deck.toString());
-console.log(deck.cards.length, 'cards');
-
-// Create a shoe
-var shoe = new ritz.Shoe();
-shoe.shuffle();
-console.log(shoe.toString());
-console.log(shoe.cards.length, 'cards');
-*/
+var CLITable = require('cli-table');
+var charm = require('charm')();
+charm.on('^C', process.exit);
+charm.pipe(process.stdout);
+charm.reset();
 
 var player = new Player();
 player.name = 'Alice';
 player.balance = 10000;
 
-var player2 = new Player();
+var player2 = new SmartPlayer();
 player2.name = 'Bob';
 player2.balance = 10000;
 
-var game;
-function newGame () {
-  game = new Game();
-  player.join(game);
-  player2.join(game);
-  game.on('over', function () {
-    newGame();
-  });
-  game.play();
-}
+var table = new Table();
+table.logger = logger;
+player.join(table);
+player2.join(table);
 
-newGame();
+function logger () {
+  var clitable = new CLITable({
+    head: [
+      'player',
+      'hand',
+      'value',
+      'status',
+      'balance',
+      'hands played'
+    ],
+    colWidths: [
+      10,
+      20,
+      10,
+      10,
+      10,
+      10
+    ]
+  });
+
+  clitable.push([
+    'Dealer',
+    table.game.dealer.hands[0].toString(),
+    table.game.dealer.hands[0].getValue(),
+    table.game.dealer.hands[0].status,
+    table.game.dealer.balance,
+    table.game.round
+  ]);
+
+  table.game.players.forEach(function (player) {
+    if (player.isDealer) {
+      return;
+    }
+
+    clitable.push([
+      player.name,
+      player.hands[0].toString(),
+      player.hands[0].getValue(),
+      player.hands[0].status,
+      player.balance,
+      player.handsPlayed
+    ]);
+  });
+
+  charm.position(0, 0);
+  charm.write(clitable.toString());
+  charm.position(0, 0);
+  charm.write(table.gameCount.toString());
+
+/*
+var dealerShouldHave = 0;
+table.players.forEach(function (player) {
+  if (player.isDealer) {
+    return;
+  }
+
+  dealerShouldHave += (10000 - player.balance);
+});
+charm.position(63, 4);
+charm.write(dealerShouldHave + ' expected');
+*/
+}
